2380ec376f91face3b35c07fc6d7336076a41798
Hans Czajkowski Jørgensen <hans.c.jorgensen@gmail.com>
2020-03-07 20:39:09 +0100

    Initial commit

---
+++ tekno-dk-custom-post-types.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Plugin Name: Tekno.dk custom post types
+ * Description: Custom post types and their relations for tekno.dk
+ * Version: 0.0.1
+ * Plugin URI: https://github.com/teknologi/tekno-dk-custom-post-types
+ * Author: Hans Czajkowski Jørgensen
+ */
